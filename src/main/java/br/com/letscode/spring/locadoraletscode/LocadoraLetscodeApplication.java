cd4446f63087320fe0cc118eab883cebd6270463
Graciele de Oliveira <oliveiragracie@gmail.com>
2021-11-06 19:40:36 -0300

    Criação da estrutura do projeto Locadora LetsCode

---
+++ src/main/java/br/com/letscode/spring/locadoraletscode/LocadoraLetscodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.spring.locadoraletscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraLetscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocadoraLetscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/spring/locadoraletscode/LocadoraLetscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.spring.locadoraletscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraLetscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
